d2ae19e9a1d2b4eef15e6a612d26ceb3e65a83a5
Bimbola <etooluwa200@gmail.com>
2022-09-17 07:27:51 +0100

    Made the alien move in both ways

--- alien_in.py
+++ alien_in.py
@@ -15,14 +15,32 @@ class Alien_invasion:
         pygame.display.set_caption("alien invasion")
         self.ship = Ship(self)
 
+
     def run_game(self):
         """main loop for the game"""
         event = ''
         while event != pygame.QUIT:
+            self._check_updates()
+            self._update_screen()
+            self.ship.update_movement()
+    def _check_updates(self):
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    #To move the ship to the right and left as it is been pressed down
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    #To stop the ship once the user stops pressing the right or left key
+                   if event.key == pygame.K_RIGHT:
+                       self.ship.moving_right = False
+                   if event.key == pygame.K_LEFT:
+                       self.ship.moving_left = False
+    def _update_screen(self):
             #set background color for every page
             self.screen.fill(self.setting.bg_color)
             self.ship.blitme()
--- ship.py
+++ ship.py
@@ -8,17 +8,27 @@ class Ship:
         """initialize the ship and set its position"""
         self.screen = ai.screen
         self.get_screen_rect = ai.screen.get_rect()
+        #flag for moving the ship to the right direction
+        self.moving_right = False
+        self.moving_left = False
 
         #load the image and get its surface rect
-        self.imagee = pygame.image.load('images/ship.bmp')
-        self.get_image_rect = self.imagee.get_rect()
+        self.image = pygame.image.load('images/ship.bmp')
+        self.get_image_rect = self.image.get_rect()
         # make all new ships be formed at the mid bottom of the screen
-        self.get_screen_rect.centerx = self.get_screen_rect.centerx
-        self.get_screen_rect.bottom = self.get_screen_rect.bottom - 5
-        #self.screen_rect = self.get_image_rect.midbottom
+        #self.get_screen_rect.centerx = self.get_screen_rect.centerx
+        #self.get_screen_rect.bottom = self.get_screen_rect.bottom - 5
+        self.get_image_rect.midbottom = self.get_screen_rect.midbottom
 
 
+    def update_movement(self):
+            """updates the user right key and left key movement as it is been pressed"""
+            if self.moving_right == True:
+                self.get_image_rect.x += 1
+            if self.moving_left == True:
+                self.get_image_rect.x -= 1
 
     def blitme(self):
         """draw the image at the current location"""
-        self.screen.blit(self.imagee, self.get_image_rect)
+        self.screen.blit(self.image, self.get_image_rect)
+
